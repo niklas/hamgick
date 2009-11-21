c09e865fb720723fefa3da5be563bdd381b802b5
Niklas Hofer <niklas+dev@lanpartei.de>
2009-11-21 03:53:27 +0100

    Image from Helper - not any nesting yet

--- app/helpers/basics_helper.rb
+++ app/helpers/basics_helper.rb
@@ -1,2 +1,15 @@
 module BasicsHelper
+  def image(opts={}, &block)
+    stuff = capture_haml(&block)
+    opts = opts.dup.reverse_merge({
+      :width => 64,
+      :height => 64,
+      :background_color => 'green'
+    })
+    @image = Magick::Image.new(opts[:width], opts[:height]) do
+      self.background_color = opts[:background_color]
+      self.format = 'png'
+    end
+    haml_concat @image.to_blob
+  end
 end
