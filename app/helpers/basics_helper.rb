e13b368357979aceb94356e1728d8e408ac77dff
Niklas Hofer <niklas+dev@lanpartei.de>
2009-11-21 04:12:58 +0100

    can draw a circle

--- app/helpers/basics_helper.rb
+++ app/helpers/basics_helper.rb
@@ -1,6 +1,5 @@
 module BasicsHelper
   def image(opts={}, &block)
-    stuff = capture_haml(&block)
     opts = opts.dup.reverse_merge({
       :width => 64,
       :height => 64,
@@ -10,6 +9,23 @@ module BasicsHelper
       self.background_color = opts[:background_color]
       self.format = 'png'
     end
+    block.call
     haml_concat @image.to_blob
+  end
+
+  def draw(opts={}, &block)
+    @draw = Magick::Draw.new
+    block.call
+    @draw.draw(@image)
+  end
+
+  def circle(opts={})
+    opts = opts.dup.reverse_merge({
+    })
+    origin_x = 32
+    origin_y = 32
+    perim_x  = 23
+    perim_y  = 23
+    @draw.circle(origin_x, origin_y, perim_x, perim_y)
   end
 end
