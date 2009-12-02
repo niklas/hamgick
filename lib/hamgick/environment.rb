04272162f3093edd60215d3b5281ecc78802e209
Niklas Hofer <niklas+dev@lanpartei.de>
2009-12-02 23:51:30 +0100

    played around with treetop again, but could not manage the semantic whitespace

--- lib/hamgick/environment.rb
+++ lib/hamgick/environment.rb
@@ -1,8 +1,6 @@
 module Hamgick
   class Environment < Hash
 
-    attr_accessor :drawer
-
     @@stack = []
 
     def self.start(attrs={})
@@ -19,37 +17,14 @@ module Hamgick
       @@stack.last
     end
 
+    def self.current
+      last
+    end
+
     def self.size
       @@stack.size
     end
 
-    def image
-      self[:image] ||= build_image
-    end
-
-    def stroke=(settings)
-      settings.each do |name, value|
-        case name
-        when :color
-          drawer.stroke(value)
-        else
-          raise "unknown stroke setting: '#{name}'"
-        end
-      end
-    end
-
-    private
-    def build_image
-      opts = dup.reverse_merge({
-        :width => 64,
-        :height => 64,
-        :background_color => 'green'
-      })
-      Magick::Image.new(opts[:width], opts[:height]) do
-        self.background_color = opts[:background_color]
-        self.format = 'png'
-      end
-    end
 
   end
 end
